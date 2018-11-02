0ba29b58230c251e4b23344517c6f5f5c559b1cf
LSBurchardt <l.s.burchardt@gmx.de>
2018-11-02 18:12:19 +0100

    #1 just a little structure added, lets see if I can push data

--- Read_datafile_and_plot_example.R
+++ Read_datafile_and_plot_example.R
@@ -1,11 +1,14 @@
-#load packages
+################################################################################
+# R Script for interaktive Plots, Finnmaid Data                                #
+# Autor: Jens D. Müller & Lara S. Burchardt                                    #
+# Email:                                                                       #
+################################################################################
 
+# 00: load packages -- ---------------------------------------------------------
 library(data.table)
 library(ggplot2)
 library(lubridate)
-
-
-#read complete dataset
+# 01: load data  ---------------------------------------------------------------
 
 
 setwd("C:/Mueller_Jens_Data/Projects/181102_interactive_datavisualisation_Finnmaid/data")
@@ -13,11 +16,10 @@ setwd("C:/Mueller_Jens_Data/Projects/181102_interactive_datavisualisation_Finnma
 df <- data.table(read.csv("Finnmaid_all_2003-2018.csv"))
 df$date <- ymd_hms(df$date)
 
-
+# 02: Datamanagment   -----------------------------------------------------------
 #subset data for latitude thresholds
 
 df.sub <- df[Lat > 56 & Lat<56.3]
-
 
 #calculate mean values for each transect (ID) of the ship
 
@@ -34,7 +36,7 @@ df.sub.mean <- df.sub[,.(
   by=.(ID)]
 
 
-
+# 03: Plots   -------------------------------------------------------------------
 #produce pCO2 vs time plot for subsetted and averages dataset 
 
 ggplot(df.sub.mean, aes(date, mean.pCO2, ymin=min.pCO2, ymax=max.pCO2))+
